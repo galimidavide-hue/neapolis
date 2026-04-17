86e8e1eefd6d2c6a9747ce25ce5acbbde8893cce
galimidavide-hue <galimidavide@gmail.com>
2026-04-17 21:49:21 +0200

    feat: assemble home page with all sections

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,21 @@
+import Hero from '@/components/home/Hero'
+import ChiSiamo from '@/components/home/ChiSiamo'
+import PizzeEvidenza from '@/components/home/PizzeEvidenza'
+import Filosofia from '@/components/home/Filosofia'
+import Recensioni from '@/components/home/Recensioni'
+import Gallery from '@/components/home/Gallery'
+import Contatti from '@/components/home/Contatti'
+
 export default function HomePage() {
-  return <div />
+  return (
+    <>
+      <Hero />
+      <ChiSiamo />
+      <PizzeEvidenza />
+      <Filosofia />
+      <Recensioni />
+      <Gallery />
+      <Contatti />
+    </>
+  )
 }
